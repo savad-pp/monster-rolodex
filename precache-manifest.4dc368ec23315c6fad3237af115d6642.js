a09c01f9460214677aa1b33f3b3d71b7fb85e3a4
savad-pp <savad.pp@riolabz.com>
2020-08-01 21:02:09 +0530

    Updates

---
+++ precache-manifest.4dc368ec23315c6fad3237af115d6642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbffaf8d2237e05b8d7dc17afa5af2f3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e0f7bdc9859c253fc792",
+    "url": "/monster-rolodex/static/css/main.4436a983.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f7bdc9859c253fc792",
+    "url": "/monster-rolodex/static/js/main.58bdd0a1.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
